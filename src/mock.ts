a1d2551085e78a7a59996998e7267ee5aeadef92
tsimafei.benko <tsimafei.benko@itechart-group.com>
2021-10-28 18:31:50 +0300

    refactor: move mocked data to separate module

---
+++ src/mock.ts
@@ -0,0 +1,16 @@
+const SLIDER_OPTIONS = [
+  {
+    description: '1',
+    content: '#c95656',
+  },
+  {
+    description: '2',
+    content: '#4eaf54',
+  },
+  {
+    description: '3',
+    content: '#4e88af',
+  },
+];
+
+export { SLIDER_OPTIONS };
