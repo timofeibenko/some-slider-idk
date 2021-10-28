2db5eb545c616e9982f8fefdef4b738f3c6ae1a9
tsimafei.benko <tsimafei.benko@itechart-group.com>
2021-10-28 18:36:46 +0300

    feat: implement basic slider logic

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,90 @@
-import './style.css';
+import { SLIDER_OPTIONS } from './mock';
 
-const SLIDER_OPTIONS = [
-  {
-    description: 1,
-    content: '#c95656',
-  },
-  {
-    description: 2,
-    content: '#4eaf54',
-  },
-  {
-    description: 3,
-    content: '#4e88af',
-  },
-];
+const ANIMATION_OPTIONS: KeyframeAnimationOptions = {
+  duration: 300,
+  fill: 'forwards',
+  easing: 'ease-in-out',
+};
 
 const sliderDescriptionArr = SLIDER_OPTIONS.map((option) => option.description);
 const sliderContentArr = SLIDER_OPTIONS.map((option) => option.content).reverse();
 
-const sliderContainer = document.querySelector('#slider-container')!;
-
-sliderContainer.innerHTML = `
-  <div 
-    class="slide-description-wrap" 
-    style="height: calc(${SLIDER_OPTIONS.length} * 100vh);"
-  >
-    ${sliderDescriptionArr.map((descr) => {
-      return `
-        <div class="slide-description">${descr}</div>
-      `;
-    })}
+(document.querySelector<HTMLDivElement>('#slider-container')!).innerHTML = `
+  <div class="slide-description-wrap" style="height: calc(${SLIDER_OPTIONS.length} * 100vh);">
+    ${sliderDescriptionArr
+      .map((descr) => {
+        return `
+                <div class="slide-description">${descr}</div>
+              `;
+      })
+      .join('')
+    }
   </div> 
   <div 
     class="slide-content-wrap" 
-    style="height: calc(${SLIDER_OPTIONS.length} * 100vh); transform: translateY(-200vh)"
+    style="height: calc(${SLIDER_OPTIONS.length} * 100vh); transform: translateY(calc(${(SLIDER_OPTIONS.length - 1)} * -100vh))"
   >
-    ${sliderContentArr.map((content) => {
-      return `
-          <div 
-            class="slide-content" 
-            style="background: ${content};"
-          >
-          </div>
-      `;
-    })}
+    ${sliderContentArr
+      .map((content) => {
+        return `
+              <div 
+                class="slide-content" 
+                style="background: ${content};"
+              >
+              </div>
+          `;
+      })
+      .join('')
+    }
   </div> 
- `;
+`;
 
+const slideDescriptionWrap = document.querySelector<HTMLDivElement>('.slide-description-wrap')!,
+  slideContentWrap = document.querySelector<HTMLDivElement>('.slide-content-wrap')!,
+  downBtn = document.querySelector<HTMLButtonElement>('#slider-nav .down')!,
+  upBtn = document.querySelector<HTMLButtonElement>('#slider-nav .up')!;
 
+let descrSlideIdx = 0;
+let contentSlideIdx = SLIDER_OPTIONS.length - 1;
+
+function getNextSlide() {
+  if (descrSlideIdx === SLIDER_OPTIONS.length - 1) {
+    return;
+  }
+
+  descrSlideIdx++;
+  contentSlideIdx--;
+
+  slideDescriptionWrap.animate([
+    {transform: `translateY(calc(${descrSlideIdx} * -100vh))`},
+  ], ANIMATION_OPTIONS);
+
+  slideContentWrap.animate([
+    {transform: `translateY(calc(${contentSlideIdx} * -100vh)`},
+  ], ANIMATION_OPTIONS);
+}
+
+function getPrevSlide() {
+  if (descrSlideIdx === 0) {
+    return;
+  }
+
+  descrSlideIdx--;
+  contentSlideIdx++;
+
+  slideDescriptionWrap.animate([
+    {transform: `translateY(calc(${descrSlideIdx} * -100vh))`},
+  ], ANIMATION_OPTIONS);
+
+  slideContentWrap.animate([
+    {transform: `translateY(calc(${contentSlideIdx} * -100vh)`},
+  ], ANIMATION_OPTIONS);
+}
+
+downBtn.addEventListener('click', () => {
+  getNextSlide();
+});
+
+upBtn.addEventListener('click', () => {
+  getPrevSlide();
+});
